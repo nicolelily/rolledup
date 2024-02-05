ff3826462de1543be35e44df01691f09a57d3fd0
Nicole Mark <nicolemark@gmail.com>
2024-02-05 20:42:57 +0000

    new file:   styles.css modified:   Hello.py modified:   README.md modified:   requirements.txt

--- Hello.py
+++ Hello.py
@@ -14,38 +14,21 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from streamlit_extras.let_it_rain import rain 
 
 LOGGER = get_logger(__name__)
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+st.set_page_config(
+    page_title="Rolled Up",
+    page_icon="💨",
+    layout="wide"
     )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+def example():
+    rain(
+        emoji="💸",
+        font_size=54,
+        falling_speed=5,
+        animation_length=15,
     )
-
-
-if __name__ == "__main__":
-    run()
+example()    
+st.image("https://i.postimg.cc/pVqgbMcY/DRC-PRINTS-FIRE.jpg", use_column_width=True)
